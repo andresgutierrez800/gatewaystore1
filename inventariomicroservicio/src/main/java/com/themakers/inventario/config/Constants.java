ec1e695bae63ad532a933bc143d8494aa49497d3
root <root@sergio.cbuepzxyeseuzb1dwnsupazjfa.jx.internal.cloudapp.net>
2020-05-23 23:12:52 +0000

    Initial version of inventariomicroservicio generated by JHipster-6.8.0

---
+++ inventariomicroservicio/src/main/java/com/themakers/inventario/config/Constants.java
@@ -0,0 +1,12 @@
+package com.themakers.inventario.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ inventariomicroservicio/src/main/java/com/themakers/inventario/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.themakers.inventario.config.audit;
